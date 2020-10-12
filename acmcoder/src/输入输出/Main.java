de987193de4757873887507a80a35502085145ee
HFwas <1558934404@qq.com>
2020-10-12 22:34:29 +0800

    "2020-10-12:"

--- acmcoder/src/输入输出/Main.java
+++ acmcoder/src/输入输出/Main.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n;
-		int m;
-		int[][] nums;
-		while (sc.hasNext()) {
-			n = sc.nextInt();
-			m = sc.nextInt();
-			nums = new int[n][m];
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					nums[i][j] = sc.nextInt();
-				}
-			}
-			
-			for (int i = 0; i < nums.length; i++) {
-				for (int j = 0; j < nums[0].length; j++) {
-					System.out.print(nums[i][j]+" ");
-				}
-				System.out.println();
-			}
-		}
+//		Scanner sc = new Scanner(System.in);
+//		int n;
+//		int m;
+//		int[][] nums;
+//		while (sc.hasNext()) {
+//			n = sc.nextInt();
+//			m = sc.nextInt();
+//			nums = new int[n][m];
+//			for (int i = 0; i < n; i++) {
+//				for (int j = 0; j < m; j++) {
+//					nums[i][j] = sc.nextInt();
+//				}
+//			}
+//			
+//			for (int i = 0; i < nums.length; i++) {
+//				for (int j = 0; j < nums[0].length; j++) {
+//					System.out.print(nums[i][j]+" ");
+//				}
+//				System.out.println();
+//			}
+//		}
 		
-		
+		System.out.println(2+2+"5"+2+2);
 	}
 }
