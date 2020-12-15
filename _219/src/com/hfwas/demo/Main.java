a36c8a9b9169ab5ac66e7bf2d6170b4b15e0ec43
HFwas <1558934404@qq.com>
2020-12-15 13:56:07 +0800

    "2020-12-15:leetcode219"

---
+++ _219/src/com/hfwas/demo/Main.java
@@ -0,0 +1,36 @@
+package com.hfwas.demo;
+
+import java.util.HashMap;
+
+/**
+ * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，
+ * 并且 i 和 j 的差的 绝对值 至多为 k。
+ * 示例 1:
+ * 输入: nums = [1,2,3,1], k = 3
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: nums = [1,0,1,1], k = 1
+ * 输出: true
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (Math.abs((i - map.get(nums[i]))) <= k){
+                    return true;
+                }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}
