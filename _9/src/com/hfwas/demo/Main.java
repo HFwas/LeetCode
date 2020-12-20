4bc7bf36b3745b5f913b0ef05ef182d074acbca1
HFwas <1558934404@qq.com>
2020-12-20 23:50:49 +0800

    "2020-12-20:leetcode9"

---
+++ _9/src/com/hfwas/demo/Main.java
@@ -0,0 +1,35 @@
+package com.hfwas.demo;
+
+/**
+ *判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * 示例 1:
+ * 输入: 121
+ * 输出: true
+ *
+ *  示例 2:
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x == 0) {
+            return true;
+        }
+        if (x < 0 || x % 10 == 0) {
+            return false;
+        }
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return x == reversed || x == reversed / 10;
+    }
+}
