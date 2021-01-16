a72640a4bd187d6c671afcb06dbb8110870db9ba
HFwas <1558934404@qq.com>
2021-01-16 20:24:43 +0800

    "2021-1-16:leetcode137"

---
+++ _137_只出现一次的数字II/src/src/main/java/Main.java
@@ -0,0 +1,45 @@
+package src.main.java;
+
+/**
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素。
+ * 说明：
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 示例 1:
+ * 输入: [2,2,3,2]
+ * 输出: 3
+ * 示例 2:
+ * 输入: [0,1,0,1,0,1,99]
+ * 输出: 99
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+
+    }
+
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int num : nums){
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
+    }
+
+    public int singleNumber2(int[] nums) {
+        int[] counts = new int[32];
+        for(int num : nums) {
+            for(int j = 0; j < 32; j++) {
+                counts[j] += num & 1;
+                num >>>= 1;
+            }
+        }
+        int res = 0, m = 3;
+        for(int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= counts[31 - i] % m;
+        }
+        return res;
+    }
+}
